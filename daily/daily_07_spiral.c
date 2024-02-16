d23e4636890f03d98007e6cb5f8f4d0be15523b3
frank chen <frankchen@astri.org>
2024-02-16 03:46:47 +0000

    update 2 files and create 1 file

--- daily/daily_07_spiral.c
+++ daily/daily_07_spiral.c
@@ -2,14 +2,35 @@
 
 void spiralize(unsigned n, int spiral[n][n]) {
     // mutate the spiral[][] 2D-array
+    int top = 0, left = 0, bottom = n - 1, right = n - 1;
+    while (top <= bottom && left <= right) {
+        printf("top: %d, left: %d, bottom: %d, right: %d\n", top, left, bottom, right);
+        for (int i = left; i <= right; i++) { spiral[top][i] = 1; }
+        if (++top > bottom) break;
+        spiral[top][right] = 1;
+        if (++top > bottom) break;
+
+        for (int i = top; i <= bottom; i++) { spiral[i][right] = 1; }
+        if (--right < left) break;
+        spiral[bottom][right] = 1;
+        if (--right < left) break;
+
+        for (int i = right; i >= left; i--) { spiral[bottom][i] = 1; }
+        if (--bottom < top) break;
+        spiral[bottom][left] = 1;
+        if (--bottom < top) break;
+
+        for (int i = bottom; i >= top; i--) { spiral[i][left] = 1; }
+        if (++left > right) break;
+        spiral[top][left] = 1;
+        if (++left > right) break;
+    }
 }
 
 void initialize(unsigned n, int spiral[n][n]) {
     // initialize the spiral[][] 2D-array
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            spiral[i][j] = 1;
-        }
+        for (int j = 0; j < n; j++) { spiral[i][j] = 0; }
     }
 }
 
@@ -23,16 +44,21 @@ void print_array(unsigned n, int spiral[n][n]) {
 
 int main() {
 
-    int array[][3] = {
-        { 1, 2, 3 },
-        { 4, 5, 6 },
+    int array[][4] = {
+        { 1, 2, 3, 1 },
+        { 4, 5, 6, 7 },
     };
 
-    printf("%d %d %d\n", array[0][0], array[0][1], array[0][2]);
+    printf("%p %p %p %p\n", array, array[0], array[1], array[2]);
+    printf("array[%d,%d]: %d\n", 0, 0, *((int*)array));
+    printf("array[%d,%d]: %d\n", 1, 2, *((int*)array + 1 * 4 + 2));
+    printf("array[%d,%d]: %d\n", 1, 3, *((int*)array + 1 * 4 + 3));
 
     int n = 5;
     int spiral[n][n];
 
     initialize(n, spiral);
+    // Here done
+    spiralize(n, spiral);
     print_array(n, spiral);
 }
---
+++ daily_08_count_change.c
@@ -0,0 +1,24 @@
+/*
+ * File: daily_08_count_change.c
+ * Project: c
+ * Created Date: Thu Feb 15 2024
+ * Author: Frank
+ * Last Modified: Thu Feb 15 2024
+ * Modified By: Frank
+ */
+#include <stdio.h>
+
+int count_change(int money, int n_coins, const int coins[n_coins]) {
+    if (money == 0) return 1;
+    if (money < 0) return 0;
+
+    int count = 0;
+    for (int i = n_coins; i > 0; i--) { count = count + count_change(money - coins[i - 1], i, coins); }
+    return count;
+}
+
+int main() {
+    printf("(4, [1,2])   : %d \n", count_change(4, 2, (int[]){ 1, 2 }));
+    printf("(10, [5,2,3]): %d \n", count_change(10, 3, (int[]){ 5, 2, 3 }));
+    printf("(11, [5,7])  : %d \n", count_change(11, 2, (int[]){ 5, 7 }));
+}
